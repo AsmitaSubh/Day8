53812c7312298414ef3fed924be03b4c140f5f21
AsmitaSubh <saswain92@gmail.com>
2023-06-10 17:54:27 +0530

    Distance

---
+++ Distance.java
@@ -0,0 +1,9 @@
+public class Distance
+{
+        public static void main(String[] args) {
+            int x = 3;
+            int y = 4;
+            double distance = Math.sqrt(x * x + y * y);
+            System.out.println("Distance from the origin: " + distance);
+        }
+    }
